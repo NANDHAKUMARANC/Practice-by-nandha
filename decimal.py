b55cc3491b0c9009577a1bd0412eaadb7f0fe163
NANDHAKUMARANC <nandhakumaran@anorasolutions.com>
2023-03-25 11:46:24 +0530

    DECIMAL ADDED BY NANDHA

---
+++ decimal.py
@@ -0,0 +1,10 @@
+﻿n=int(input("Enter the decimal value: "))
+binary=[]
+while(n>0):
+    if n%2==0:
+        binary.append(0)
+    else:
+        binary.append(1)
+    n=n//2
+binary=binary[::-1]
+print(binary)
